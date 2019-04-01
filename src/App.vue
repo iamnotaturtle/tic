eeaf0b804740f08c3945cb4ad98410108edf427f
Yakov Gaberman <ygaberman@factset.com>
2019-03-31 20:56:03 -0400

    feat(tictoc): adds css grid
    Removes unused files

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,84 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+    <div class="title">
+      Tic Tac Toe
     </div>
-    <router-view/>
+    <div class="grid">
+      <template v-for="(row, index) in board">
+        <div class="item" v-for="(item, key) in row" :key="index + '-' + key"
+          @click="select(item, index, key)">
+          <transition name="flip" mode="out-in">
+            <div v-if="item !== ''" class="content" :class="[getColor(item)]">
+              {{ item }}
+            </div>
+          </transition>
+        </div>
+      </template>
+    </div>
+    <div v-if="result" class="msg">
+      {{ msg }}
+    </div>
   </div>
 </template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+
+@Component
+export default class App extends Vue {
+  result: boolean = false;
+
+  grid: any[] = [
+    ['', '', ''],
+    ['', '', ''],
+    ['', '', ''],
+  ];
+
+  currentType: string = 'X';
+
+  getColor = (item: string): string => (item === 'X' ? 'green' : 'red')
+
+  get board(): any {
+    return this.grid;
+  }
+
+  get msg(): string {
+    return `Congrats! ${this.currentType} won!`;
+  }
+
+  select(item: string, row: number, column: number) {
+    if (item || this.result) {
+      return;
+    }
+
+    const currentRow = this.grid[row].slice(0);
+    currentRow[column] = this.currentType;
+    this.$set(this.grid, row, currentRow);
+    this.result = this.didWin(row, column);
+
+    if (!this.result) {
+      this.currentType = this.currentType === 'X' ? 'O' : 'X';
+    }
+  }
+
+  didWin(row: number, column: number): boolean {
+    const type = this.grid[row][column];
+
+    if (!type) {
+      return false;
+    }
+
+    const rowResult = this.grid[row].every((value: string) => value === type);
+    const columnResult = this.grid.every((gridRow: string[]) => gridRow[column] === type);
+    const leftDiagonal = this.grid.every((gridRow: string[], index) => gridRow[index] === type);
+    const rightDiagonal = this.grid.every(
+      (gridRow: string[], index) => gridRow[gridRow.length - 1 - index] === type,
+    );
+    return rowResult || columnResult || leftDiagonal || rightDiagonal;
+  }
+}
+</script>
+
 
 <style lang="scss">
 #app {
@@ -16,14 +88,52 @@
   text-align: center;
   color: #2c3e50;
 }
-#nav {
-  padding: 30px;
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
+
+.grid {
+  display: grid;
+  justify-content: center;
+  grid-template-columns: 50px 50px 50px;
+  grid-gap: 5px;
+}
+
+.item {
+  width: 50px;
+  height: 50px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  user-select: none;
+  background: white;
+  border: 1px solid darkgrey
+}
+
+.flip-enter-active {
+  transition: all .2s cubic-bezier(0,.75,.25,1); //ease-in-quad
+}
+
+.flip-leave-active {
+  transition: all .25s cubic-bezier(0,.75,.25,1); //ease-out-quad
+}
+
+.flip-enter, .flip-leave-to {
+  transform: scaleY(0) translateZ(0);
+  opacity: 0;
+}
+
+.content {
+  width: 100%;
+  height: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background: lightgreen;
+}
+
+.green {
+  background: lightgreen;
+}
+
+.red {
+  background: lightsalmon;
 }
 </style>
