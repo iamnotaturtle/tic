0441e28b1c3f097ce5e15e85483770d72b08076f
iamnotaturtle <iamnotaturtle@users.noreply.github.com>
2019-07-08 21:32:17 -0400

    chore(build): adds dist directory

---
+++ docs/precache-manifest.a8a673815e3d79005ec135f643eaadb5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "9fa0a009b775d7f02f52",
+    "url": "/js/chunk-vendors.75903d69.js"
+  },
+  {
+    "revision": "6ad20b722f3b09b52f6a",
+    "url": "/js/app.f4c43763.js"
+  },
+  {
+    "revision": "e934eb70fd7ec8febcfe85032fbccf66",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6ad20b722f3b09b52f6a",
+    "url": "/css/app.14dc14f1.css"
+  }
+];
